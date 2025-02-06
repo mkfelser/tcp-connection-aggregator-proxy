a8bc01c262331f787ef1e255ec1fd1d7ce6c1332
Meik Felser <mkfelser@gmail.com>
2025-02-06 22:45:06 +0100

    doc: add copyright notice

--- src/tcp-connection-aggregator-proxy.c
+++ src/tcp-connection-aggregator-proxy.c
@@ -1,4 +1,25 @@
-#include <stdio.h>        // printf
+/*
+ * tcp-connection-aggregator-proxy.c
+ * 
+ * Copyright (c) 2025 Meik Felser (mkfelser)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ */
+
+#include <stdio.h>        // printf 
 #include <stdlib.h>       // exit
 #include <netinet/in.h>   // struct sockaddr_in
 #include <arpa/inet.h>    //inet_ntoa
